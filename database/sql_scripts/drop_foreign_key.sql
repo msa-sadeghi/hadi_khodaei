44bf7dcceaedd75894bd58fc73328783cd757c02
msa-sadeghi <sadeghi.msai@gmail.com>
2024-10-11 09:37:26 +0330

    a

---
+++ database/sql_scripts/drop_foreign_key.sql
@@ -0,0 +1,3 @@
+-- DROP foreign key order_ibfk_1;
+-- ALTER table online_shop.order DROP INDEX user_id;
+-- ALTER table online_shop.order ADD foreign key(user_id) references user(id);
--- database/sql_scripts/stored_procedure.sql
+++ database/sql_scripts/stored_procedure.sql
@@ -18,3 +18,16 @@ END; $$;
 
 CALL get_clients_by_state('CA', 'test');
 fetch all in "test";
+
+
+
+CREATE PROCEDURE get_clients_by_state_2(state char(2), INOUT r refcursor)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	open r for SELECT * FROM public.clients c
+	WHERE c.state = NULLIF(state, c.state);
+END; $$;
+
+CALL get_clients_by_state(NULL, 'test');
+fetch all in "test";
