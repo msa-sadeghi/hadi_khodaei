83cc11c43b22818a4517fb60dceaa6640682193d
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-08-01 09:57:17 +0330

    codes

---
+++ javascript/session1/1/script.js
@@ -0,0 +1,121 @@
+// alert("hi")
+// var userCount = 1000;
+// var myName = "artin"
+// var isOnline = true
+// var isOnline = false
+/* alert(typeof userCount)
+alert(typeof myName)
+alert(typeof isOnline) */
+// var userinfo = prompt("enter the name:")
+// alert(userinfo)
+// var userinfo = prompt("enter the number:")
+// var userNumber = Number(userinfo)
+// var userinfo = prompt("enter the number:")
+// var userNumber = +userinfo
+// console.log(typeof userNumber)
+// console.log(userNumber)
+// var userAge = 37
+// var  userAgeString = String(userAge)
+// console.log(typeof userAge)
+// console.log(typeof userAgeString)
+// var is_ok = false
+// var isOkString = String(is_ok)
+// console.log(typeof isOkString)
+
+// var name = "sara"
+// var lastName = "sabour"
+
+// console.log("name is " + name + "\nand lastname is " + lastName)
+// console.log(`name is ${name} \nand lastname is  ${lastName}`)
+
+// var str1 = "blalalal"
+// var num1 = 12
+// var num2 = 4
+
+// var  temp = str1/num1
+// console.log(typeof temp)
+// console.log(typeof NaN)
+
+// console.log("12" + 4)
+// console.log(12 + "4")
+// console.log("12" * 4)
+// console.log(12 * "4")
+// console.log("12" / "4")
+// console.log("12" / 4)
+// console.log("aaaa" / 4)
+// var str1 = "blalal"
+// var str2 = "ggggg"
+
+
+// console.log(isNaN(str1 + str2))
+
+
+// var x = 12
+// var y = 3
+// var k = 4
+// var z = "12"
+
+// console.log(x == y)
+// console.log(x == z)
+// console.log(y * k == x)
+// console.log(y * k == z)
+// console.log(x / y == k)
+// console.log(z / y == k)
+
+// console.log(x === z)
+
+
+// var x = 12;
+// var num2 = 4
+// if (x > num2){
+//     console.log("yes")
+// } else{
+//     console.log("no")
+// }
+
+
+var x = Number(prompt("enter ... ", 0))
+// سن کاربر را دریافت کنید.
+// اگر سن کاربر کمتر از 18 بود،
+//  به او پیغام "شما مجاز به ورود نیستید"
+//  را نمایش بدهید و در غیر این صورت پیغام "ورود به پنل" را نمایش بدهید.
+
+
+// دو عدد را از کاربر دریافت کرده و عدد اول را به توان دومی برسانید و حاصل را نمایش دهید
+
+
+// یک زمان را بر حسب دقیقه از کاربر دریافت کرده و آن را به ساعت تبدیل کنید و سپس نمایش دهید
+
+
+var num1 = 12;
+var num2 = 4;
+var num3 = 3;
+
+if (num2 * num3 === 10) {
+  alert("ضربشون برابره با 10");
+} else if (num2 * num3 === 11) {
+  alert("ضربشون برابره با 11");
+} else if (num2 * num3 === 13) {
+  alert("ضربشون برابره با 12");
+} else {
+	alert("ضربشون برابر با هیچکدوم نیست :|")
+}
+// از کاربر سن و جنسیت را دریافت کنید.
+// در صورتی که جنسیت کاربر مونث بوده یا سن کمتر از 18 را دارد، به اون خطا نمایش داده
+// و در غیر این صورت به اون مجوز ورود به پنل را نمایش دهید.
+var num1 = 12;
+var num2 = 4;
+var num3 = 3;
+if (num1 * num3 === 10 || num1 * num3 === 11) {
+  alert("ضربشون 10 یا 11 هست");
+} else if (num2 * num3 === 14 || num2 * num3 === 12) {
+  alert("ضربشون 14 یا 12 میشه");
+} else if (num2 * num3 === 13 || num1 * num3 === 15) {
+  alert("ضربشون 13 یا 15 میشه");
+}
+
+
+// OR || یا
+// true || true => true
+// true || false => true
+// false || false => false
