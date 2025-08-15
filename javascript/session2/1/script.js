eeb67c044f3c44e2b516f861297c8cfc0226f14d
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-08-15 09:54:39 +0330

    exam

---
+++ javascript/session2/1/script.js
@@ -0,0 +1,143 @@
+// var num1 = 4
+// var num2 = 5
+// if(num1 === 4 && num1 * num2 === 20){
+//     alert(`result is ${num1} * ${num2} = ${num1 * num2}`)
+// }else if (num2 === 4 && num1 * num2 === 20){
+//     alert(`result is ${num1} * ${num2} = ${num1 * num2}`)
+// }else{
+//     alert("nothing")
+// }
+
+// ||
+// true && true => true
+// true && false => false
+// false && false => false
+
+
+// var my = true
+// console.log("my " + my)
+// console.log("my " + !my)
+// console.log("my " + !!my)
+
+// var num1 = 4
+// var num2 = 3
+// if(num1 * num2 === 12){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+
+// num1 * num2 === 16 ? console.log("yes") : console.log("no")
+
+// معدل کاربر را دریافت کرده و سپس سطح او را تعیین کنید
+
+// A برای معدل 18 الی 20 سطح
+// B برای معدل 15 الی 17 سطح
+// C برای معدل 12 الی 14 سطح
+// معدل کمتر از 12 مشروط میشه :(
+
+
+// var num1 = 3
+// var num2 = 4;
+// var num3 = num1 * num2
+
+// switch(num3){
+//     case "10":
+//     case "11":
+//         console.log("10")
+//         break
+//     case 11:
+//         console.log("11")
+//         break
+//     case 12:
+//         console.log("12")
+//         break
+//     case 13:
+//         console.log("13")
+//         break
+
+//     default:
+//         console.log("default")
+//         break
+// }
+
+// myFunction()
+// function myFunction(){
+//     var num1 = 2
+//     var num2 = 8
+//     var res = num1 + num2
+//     console.log("res" + res)
+// }
+
+
+// var greet = function(){
+//     console.log("hello")
+// }
+// greet()
+// تابعی بنویسید که عددی را دریافت کرده 
+// و زوج یا فرد بودن آن را نمایش دهد
+
+
+// function my(text){
+//     console.log("test " + text)
+// }
+
+// my("laalal")
+
+// تابعی بنویسید که سه عدد رو بگیره 
+// و میانگین اونا رو محاسبه کرده و نشون بده
+
+// function sumValue (num1 , num2 )  {
+
+// 	if (num1 == undefined) {
+// 		num1 = 1
+// 	}
+// 	if (num2 == undefined) {
+// 		num2 = 2
+// 	}
+// 	var result = num1 + num2
+// 	alert("Result= " + result)
+// }
+
+// sumValue()
+
+// function sumNumbers (num1, num2) {
+// 	var result = num1 + num2
+// 	// alert("Result = " + result)
+// 	return result
+// }
+
+// var sumResult = sumNumbers(2, 8)
+
+// alert("sumResult = " + sumResult)
+
+/////////////////////////////////////
+
+
+// var number1 = prompt('Enter First Number ...')
+// var number2 = prompt('Enter Second Number ...')
+
+
+// function showPower (num1, num2) {
+
+// 	// alert("num1 = " + num1)
+// 	return num1 ** num2
+// 	// return 10
+// }
+
+// var powerResult = showPower(number1, number2)
+
+// alert('powerResult = ' + powerResult)
+
+
+// یک برنامه ثبت نام بنویسید
+
+// به این شکل که کاربر برای ثبت نام 
+// در سایت باید یوزرنیم و پسورد را ثبت کند
+
+// محدودیت موجود این است که یوزرنیم حداقل باید 3 کاراکتر 
+// و پسورد حداقل باید 8 کاراکتر داشته باشد
+
+// در غیر این صورت پیغام مناسب را به کاربر نمایش دهید
+
+// (: موفق باشید 
---
+++ javascript/session3/1/script.js
@@ -0,0 +1,106 @@
+// var text = "I love js"
+
+// console.log(text[0])
+// console.log(text.charAt(2))
+// console.log(text.charCodeAt(2))
+// console.log(text +  " and react")
+// console.log(text.concat(" and react"))
+// console.log(text.trim())
+// console.log(text.toUpperCase())
+// console.log(text.search("js"))
+// console.log(text.indexOf("js"))
+// console.log(text.includes("js"))
+
+
+// 1.
+// یک سیستم لاگین پیاده سازی کنید
+// به این صورت که از کاربر یوزرنیم رو میگیرین 
+// و چک می کنین لاگین هست یا نه
+// فقط کاربر با یوزرنیم
+// ali
+// میتونه لاگین کنه
+
+// نکته: این پروژه نباید به حروف کوچیک و بزرگ حساب باشد
+
+
+
+
+// 2.
+// تفاوت متد های
+// search و indexOf
+
+
+// const str = "Hello world"
+
+// console.log(str.slice(-6, -1))
+// console.log(str.substring(-6, -1))
+
+
+// console.log(Math.PI)
+// console.log(Math.pow(2,3))
+// console.log(Math.abs(-1))
+// console.log(Math.min(-1, 12, 45, 67))
+// console.log(Math.floor(12.1))
+// console.log(Math.floor(-12.9))
+// console.log(Math.trunc(-12.9))
+// console.log(Math.round(12.9))
+// console.log(Math.round(12.2))
+// console.log(Math.ceil(12.2))
+
+// پروژه ای بنویسین که کد کپچای 5 رقمی به صورت تصادفی تولید کند
+
+
+// var number = 5
+// number += 1
+// number -= 1
+// number *= 1
+// number /= 1
+// number **= 1
+// number++;
+// number--;
+
+// ++number
+// --number
+// در مورد تفاوت های حالتی که -- و
+//  ++ پشت یا مقابل متغیر قرار می گیرند، سرچ و مطالعه کنید
+
+
+
+// var x = 0;
+// console.log(x++)
+
+for (var i = 0; i< 5; i++){
+    console.log("ok" + i)
+}
+
+var name = "nikan"
+
+for(var i = 0; i < name.length; i++){
+    console.log(name[i])
+}
+
+// اعداد زوج بین 0 تا 100 را در کنسول چاپ کنید
+
+
+
+// قیمت 5 محصول را از کاربر گرفته
+//  و مجموع مبلغ کل سبد خرید رو نمایش دهید
+
+
+// از کاربر 5 عدد را دریافت کرده 
+// و میانگین آن ها را به کاربر نمایش دهید
+
+
+// عددی را از کاربر دریافت کرده 
+// و سپس مجموع رقم های آن را محاسبه کنید.
+
+// :به عنوان مثال برای عدد 142 به صورت زیر محاسبه می شود
+
+// 1 + 4 + 2 = 7
+
+
+// عددی را از کاربر دریافت کرده و سپس تعداد رقم های آن را نمایش دهید.
+
+// به عنوان مثال برای عدد 142 عدد 3 به عنوان خروجی نمایش داده می شود.
+//  چون عدد 
+// 143 از 3 رقم تشکیل شده است.
