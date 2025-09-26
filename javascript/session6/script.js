b568b62e20a6a94d1cd542f429f20dc9627a02cd
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-09-26 11:31:19 +0330

    lamp

--- javascript/session6/script.js
+++ javascript/session6/script.js
@@ -1,16 +1,27 @@
 const element = document.getElementsByName('test')[0]
-console.log(element.innerText)
-const element2 = document.getElementsByTagName('li')[1]
-console.log(element2.innerText)
-const element3 = document.getElementsByClassName('list-item')[2]
-console.log(element3.innerText)
-const element4 = document.querySelectorAll('.list-item')[3]
-console.log(element4.innerText)
-const element5 = document.querySelector('.list-item#tt')
-console.log(element5.innerText)
+const x = element.value
+console.log(x)
+const btn = document.getElementsByTagName('button')[0]
 
-const h3Element = document.querySelector('h3')
-document.addEventListener('click', function(){
-
-    h3Element.setAttribute('class', 'test')
+const ulEl = document.getElementsByTagName('ul')[0]
+const inputElem = document.querySelector('input')
+btn.addEventListener("click", function(){
+    var elem1 = document.createElement("li")
+    elem1.setAttribute('id', "test2")
+    elem1.setAttribute('class', "test")
+    elem1.innerHTML= inputElem.value
+    elem1.style.color = 'green'
+    var elem = document.createElement("li")
+    // elem.setAttribute('id', "test2")
+    // elem.setAttribute('class', "test")
+    elem.innerHTML= inputElem.value
+    // elem.style.color = 'green'
+    // elem.classList.add('test')
+    // ulEl.append(elem1, elem)
+    ulEl.append(elem)
 })
+
+// یک عکس لامپ و یک دکمه داشته باشید
+// با کلیک روی دکمه، لامپ روشن و خاموش بشه
+
+// (src تغییر)
