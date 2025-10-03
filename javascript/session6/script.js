1eb8f963b5b1231bd98d8fb7729bb4df0af3df83
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-10-03 11:40:42 +0330

    c

--- javascript/session6/script.js
+++ javascript/session6/script.js
@@ -1,27 +1,8 @@
-const element = document.getElementsByName('test')[0]
-const x = element.value
-console.log(x)
-const btn = document.getElementsByTagName('button')[0]
+const input = document.getElementsByTagName('input')[0]
+const span = document.querySelector('span')
 
-const ulEl = document.getElementsByTagName('ul')[0]
-const inputElem = document.querySelector('input')
-btn.addEventListener("click", function(){
-    var elem1 = document.createElement("li")
-    elem1.setAttribute('id', "test2")
-    elem1.setAttribute('class', "test")
-    elem1.innerHTML= inputElem.value
-    elem1.style.color = 'green'
-    var elem = document.createElement("li")
-    // elem.setAttribute('id', "test2")
-    // elem.setAttribute('class', "test")
-    elem.innerHTML= inputElem.value
-    // elem.style.color = 'green'
-    // elem.classList.add('test')
-    // ulEl.append(elem1, elem)
-    ulEl.append(elem)
+const btn = document.querySelector('button')
+btn.addEventListener('click', function(){
+    // span.setAttribute('class', 'test')
+    span.classList.toggle('test')
 })
-
-// یک عکس لامپ و یک دکمه داشته باشید
-// با کلیک روی دکمه، لامپ روشن و خاموش بشه
-
-// (src تغییر)
