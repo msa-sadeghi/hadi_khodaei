fee7962f21e0909e42ec0963e2fdcb64c7a99002
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-10-10 11:06:20 +0330

    d

--- javascript/session6/script.js
+++ javascript/session6/script.js
@@ -1,4 +1,7 @@
-let data = {
-    iran:['tehran', 'shiraz'],
-    terkey:['tehran', 'shiraz'],
+let citiesData = {
+  tehran: {city: 'Tehran', temp: 12, weather: 'Sunny', humidity: 23, windSpeed: 32},
+  shiraz: {city: 'Shiraz', temp: 9, weather: 'windy', humidity: 12, windSpeed: 14},
+  tabriz: {city: 'Tabriz', temp: 1, weather: 'rainy', humidity: 43, windSpeed: 12},
+  mashhad: {city: 'Mashhad', temp: 16, weather: 'snowy', humidity: 7, windSpeed: 24},
+  esfahan: {city: 'Esfahan', temp: 23, weather: 'Sunny', humidity: 15, windSpeed: 18},
 }
