8e434b51581d541ddc4885d7f68ac564419c1071
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-10-10 09:13:18 +0330

    vv

--- javascript/session6/script.js
+++ javascript/session6/script.js
@@ -1,8 +1,4 @@
-const input = document.getElementsByTagName('input')[0]
-const span = document.querySelector('span')
-
-const btn = document.querySelector('button')
-btn.addEventListener('click', function(){
-    // span.setAttribute('class', 'test')
-    span.classList.toggle('test')
-})
+let data = {
+    iran:['tehran', 'shiraz'],
+    terkey:['tehran', 'shiraz'],
+}
