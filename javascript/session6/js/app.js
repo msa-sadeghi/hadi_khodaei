0a81555a72a16e8968b90c115f4f11f70390ef12
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-12-19 11:12:36 +0330

    c

--- javascript/session6/js/app.js
+++ javascript/session6/js/app.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
+document.documentElement.style.setProperty('--test-color', 'red')
 
-
-let allImages = ["",""]
-let index = 0
-
-let img = document.querySelector('img')
-function NextBtn(){
-    img.setAttribute('src', allImages[index])
-}
-
-
-function PrevBtn(){
-
-}
-setInterval(NextBtn, 3000)
-=======
-const boxElem = document.querySelector('.box')
-
-console.log(boxElem.style)
-
-let boxStyles = getComputedStyle(boxElem)
-console.log(boxStyles.backgroundColor)
-console.log(boxStyles.getPropertyValue('width'))
-
-
->>>>>>> 27073f3d2e2bc68f38e4d13561e38d1f03f9251b
+console.log(document.documentElement.style.getPropertyValue('--test-color'))
